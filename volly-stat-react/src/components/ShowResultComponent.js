5974957d500b0a61c38648534b5ad89fbb5e450e
Poyan Gerami <poyan.gerami@gmail.com>
2018-12-01 01:27:46 +0100

    implemented game result

--- volly-stat-react/src/components/ShowResultComponent.js
+++ volly-stat-react/src/components/ShowResultComponent.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import GameResult from './GameResult'
 class ShowResultComponent extends Component {
   render() {
-    let length = this.props.results.length;
     return (
       <div id="accordion">
-        {this.props.results.map((result, i) => <GameResult gameresult={result} isLast={length === i + 1} key={result.id} />)}
+        {this.props.results.map((result, i) => <GameResult gameresult={result} isLast={i === 0} key={result.id} />)}
       </div>
 
     );
